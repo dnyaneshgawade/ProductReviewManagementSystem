44021798065a669449dcc28806e87f3bcb548c51
dnyana <dnyaneshgawade903@gmail.com>
2021-09-04 08:25:23 +0530

    [Dnyanesh] Add :Wellcome message

---
+++ ProjectReviewManagementSystem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjectReviewManagementSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wellcome to Product Review Management system!");
+        }
+    }
+}
