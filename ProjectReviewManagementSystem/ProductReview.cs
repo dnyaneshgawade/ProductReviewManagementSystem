42999c7b42506e8a5656201a0f40f17ba735ab1a
dnyana <dnyaneshgawade903@gmail.com>
2021-09-04 20:58:55 +0530

    [Dnyanesh] Add :Creating DataTable For  Product Review Management

--- ProjectReviewManagementSystem/ProductReview.cs
+++ ProjectReviewManagementSystem/ProductReview.cs
@@ -9,6 +9,7 @@ namespace ProjectReviewManagementSystem
         public int ProductId { get; set; }
         public int UserId { get; set; }
         public int Rating { get; set; }
+        public string Review { get; set; }
         public bool IsLike { get; set; }
     }
 }
