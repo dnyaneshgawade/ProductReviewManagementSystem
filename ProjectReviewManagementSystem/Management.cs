5f83b0dd87874a247f4afd27ec6ae8af8aca6009
dnyana <dnyaneshgawade903@gmail.com>
2021-09-04 10:02:15 +0530

    [Dnyanesh] Add :Retrive Perticular Fields From  Product Review Management

--- ProjectReviewManagementSystem/Management.cs
+++ ProjectReviewManagementSystem/Management.cs
@@ -43,23 +43,35 @@ namespace ProjectReviewManagementSystem
             }
         }
 
+        public void RetrivePerticularFields(List<ProductReview> review)
+        {
+            var recordedData = from list in review
+                               select new { list.ProductId, list.Rating };
+            foreach (var item in recordedData)
+            {
+                Console.WriteLine("Product Id : " + item.ProductId + "\t\tProduct Rating : " + item.Rating);
+            }
+        }
+
         public void Operations()
         {
-            Program program = new Program();
             while (choice != 10)
             {
-                Console.WriteLine("\n Enter 1 for Display top Three records\n Enter 2 for Display Selected records\n Enter 3 for Display Count of Records For ProductID \n Enter 10 for exit ");
+                Console.WriteLine("\n Enter 1 for Display top Three records\n Enter 2 for Display Selected records\n Enter 3 for Display Count of Records For ProductID\n Enter 4 for Display Perticular Fields \n Enter 10 for exit ");
                 choice = Convert.ToInt32(Console.ReadLine());
                 switch (choice)
                 {
                     case 1:
-                        this.TopRecords(Program.list);
+                        TopRecords(Program.list);
                         break;
                     case 2:
                         RetrivePerticularRecord(Program.list);
                         break;
                     case 3:
                         RetriveCount(Program.list);
+                        break;
+                    case 4:
+                        RetrivePerticularFields(Program.list);
                         break;
                     default:
                         Console.WriteLine("Enter wrong input");
